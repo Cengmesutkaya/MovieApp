c987d3df58b2e8d883a1897cd4154c4dea4ddc8c
Mesut KAYA <mesutkaya@anadolu.edu.tr>
2020-12-08 08:56:58 +0300

    Startup file is edited.

--- MovieApp/MovieApp/Startup.cs
+++ MovieApp/MovieApp/Startup.cs
@@ -18,13 +18,11 @@ namespace MovieApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,9 +34,7 @@ namespace MovieApp
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
             app.UseStaticFiles();
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
